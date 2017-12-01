f8be2ce702aa1fea4811a938ca7dfc8a4e9cd62d
Deyan Vladkov <deian.vladkov@gmail.com>
2017-12-01 16:14:40 +0000

    Added delete poll API route

--- routes/routes.js
+++ routes/routes.js
@@ -90,24 +90,16 @@ module.exports = function (app, passport) {
         });
     });
 
-    app.get('/profile/:poll', isLoggedIn, function (req, res) {        
-        // If poll param json is set to 1 (poll?json=1) return JSON response 
-        if(req.query.json == 1) {            
-            // Find all documents, hide _id, pid, voter_id, sort by latest poll
-            Poll.find({}, {'_id': 0, 'pid': 0, voter_id: 0}, {sort: {'_id': -1}}, function (err, polls) {
-            if (err) throw err;
-            //console.log(polls);
-            return res.json(polls);
-        });
-            
-        } else {
-            res.render('poll', {
+    app.get('/profile/poll', isLoggedIn, function (req, res) {
+
+        res.render('poll', {
             user: req.user
-            });
-        }
-       
+        });
+
+
     });
 
+    // Create new poll
     app.post('/profile/poll', isLoggedIn,
         form(
             field("question").trim().required(),
@@ -115,7 +107,7 @@ module.exports = function (app, passport) {
             field("option2").trim().required()
         ),
         // TODO: dynamic input verification
-             
+
         function (req, res) {
             //console.log("POST Poll: user id", req.user);
 
@@ -133,9 +125,9 @@ module.exports = function (app, passport) {
             pollOptions[option2] = 0;
 
             var newPoll = new Poll({
-                owner_id: req.user.id,           
+                owner_id: req.user.id,
                 question: req.body.question,
-                options: [pollOptions]            
+                options: [pollOptions]
             });
             console.log(newPoll)
             newPoll.save(function (err, poll) {
@@ -147,65 +139,118 @@ module.exports = function (app, passport) {
             });
         }
     );
-    
+
     // Public polls
     app.get('/polls', function (req, res) {
-        // If poll param json is set to 1 (poll?json=1) return JSON response 
-        if(req.query.json == 1) {            
-            // Find all documents, hide _id, pid, voter_id, sort by latest poll
-            Poll.find({}, {'_id': 0, 'pid': 0, voter_id: 0}, {sort: {'_id': -1}}, function (err, polls) {
-            if (err) throw err;
-            //console.log(polls);
-            return res.json(polls);
-        });
-            
-        } else {
-            res.render('polls', {
+        res.render('polls', {
             user: req.user
-            });
-        }
-    });    
-    
-    
+        });
+    });
+
+
     // API routes
-    
+
     app.get('/api', function (req, res) {
         res.render('api', {
             message: req.flash('message')
         });
     });
-    
+
+    // GET all polls
     app.get('/api/poll', function (req, res) {
         // Find all documents, hide _id, pid, owner_id, voter_id, sort by latest poll
-        Poll.find({}, { '_id': 0, pid: 0, owner_id: 0, voter_id: 0}, {sort: {'_id': -1}}, function (err, polls) {
+        Poll.find({}, {
+            '_id': 0,
+            pid: 0,
+            owner_id: 0,
+            voter_id: 0
+        }, {
+            sort: {
+                '_id': -1
+            }
+        }, function (err, polls) {
             if (err) throw err;
             //console.log(polls);
             res.json(polls);
         });
     });
-    
+
+    // GET poll by id
     app.get('/api/poll/:id', function (req, res) {
-         //console.log(req.params.id, "PID");
-         if(req.params.id > 0 ) {         
+        //console.log(req.params.id, "PID");
+        if (req.params.id > 0) {
             // Find poll by id
-            Poll.find({pid:req.params.id}, {'_id': 0, voter_id: 0}, function (err, polls) {
-                if (err) throw err;                
+            Poll.find({
+                pid: req.params.id
+            }, {
+                '_id': 0,
+                voter_id: 0
+            }, function (err, polls) {
+                if (err) throw err;
                 if (Object.keys(polls).length > 0) { // poll object is not empty
                     return res.json(polls);
                 } else {
-                    var response = {'Poll not found': 0};
+                    var response = {
+                        'not found': 0
+                    };
                     res.json(response);
                 }
             });
-         } else {
-             var response = {'Poll not found': 0};
-             res.json(response);
-         }
+        } else {
+            var response = {
+                'not found': 0
+            };
+            res.json(response);
+        }
     });
 
-    
-    
-    
+    // DELETE poll by id
+    // Todo check if user is logged
+    app.delete('/api/poll/:id', function (req, res) {
+        // TODO check if user is owner of the poll
+        //console.log(req.user, "USER");
+        var testOwnerId = 6;
+        if (req.params.id > 0) {
+            // Find poll by id
+            Poll.find({
+                pid: req.params.id
+            }, {
+                '_id': 0,
+                voter_id: 0
+            }, function (err, polls) {
+                if (err) throw err;
+                if (Object.keys(polls).length > 0) { // poll object is not empty
+                    //console.log(polls);
+                    //console.log(testOwnerId, ' ', polls[0].owner_id);
+                    if (polls[0].owner_id === testOwnerId) {
+                        //console.log('DELETE Poll');
+                        Poll.remove({
+                            pid: req.params.id
+                        }, function (err) {
+                            if (err) return handleError(err);
+                            var response = {
+                                'deleted': 1
+                            };
+                            return res.json(response);
+                        });
+                    }
+
+                } else {
+                    var response = {
+                        'not found': 0
+                    };
+                    res.json(response);
+                }
+            });
+        } else {
+            var response = {
+                'not found': 0
+            };
+            res.json(response);
+        }
+    });
+
+
     app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
